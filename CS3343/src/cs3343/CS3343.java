066546b4b683434186849d5983ba0935c2456be5
John_YOU <yhlfc@hotmail.com>
2016-10-16 23:49:47 +0800

    project

---
+++ CS3343/src/cs3343/CS3343.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs3343;
+
+/**
+ *
+ * @author youhan
+ */
+public class CS3343 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
